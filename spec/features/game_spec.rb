87f01127a24d9eb1e5ce14b2794a57ce85dba724
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-17 14:03:38 +0100

    User enters name to play button added

--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -4,3 +4,12 @@ feature 'Enter page' do
     expect(page).to have_content ('Welcome to Rock, Papers and Scissors game!')
   end
 end
+
+feature "User enters name" do
+  scenario 'User enters name to play' do
+    visit('/')
+    fill_in :name, with: 'Mai'
+    click_button "Submit"
+  end
+end
+  
