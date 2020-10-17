6d8371a0ef4210aa487a8d9090c736f926606216
Maiara Rodrigues <maiarinhah.eu@gmail.com>
2020-10-17 16:33:19 +0100

    tests passing for entering new page

--- app.rb
+++ app.rb
@@ -2,10 +2,20 @@ require 'sinatra'
 require 'shotgun'
 
 class Rps < Sinatra::Base
-    set :session_secret, 'super secret'
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/' do
     erb(:index)
+  end
+
+  post '/names' do
+    session[:name] = params[:name]
+    redirect '/play'
+  end 
+
+  get '/play' do
+    erb(:play)
   end
 
 end 
--- spec/features/game_spec.rb
+++ spec/features/game_spec.rb
@@ -1,15 +1,29 @@
 feature 'Enter page' do
   scenario 'User enters page with welcome message' do
     visit('/')
-    expect(page).to have_content ('Welcome to Rock, Papers and Scissors game!')
+    expect(page).to have_content 'Welcome to Rock, Papers and Scissors game!'
   end
 end
 
-feature "User enters name" do
+feature 'User enters name' do
   scenario 'User enters name to play' do
     visit('/')
     fill_in :name, with: 'Mai'
     click_button "Submit"
   end
 end
+
+# feature 'Shows the name of the player' do
+#   scenario 'shows user name' do
+#     visit('/')
+#     fill_in :name, with: 'Mai'
+#     click_button "Submit"
+#   end
+# end
   
+feature 'Welcome to the game' do
+  scenario 'Entering the next page' do
+    visit('/play')
+    expect(page).to have_content 'Hi, Mai. Choose your weapon:'
+  end 
+end
